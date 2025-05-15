1c8ab1eac61689a7c57e40e608b4e80b1a784c85
sugonamu <je0jwff@gmail.com>
2025-05-15 15:20:04 +0100

    initial publisher code

---
+++ target/debug/deps/publisher.d
@@ -0,0 +1,5 @@
+C:\Users\apoll\OneDrive\Desktop\SEM4\RPL\publisher\target\debug\deps\publisher.exe: src\main.rs
+
+C:\Users\apoll\OneDrive\Desktop\SEM4\RPL\publisher\target\debug\deps\publisher.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/publisher.d
@@ -0,0 +1,1 @@
+C:\Users\apoll\OneDrive\Desktop\SEM4\RPL\publisher\target\debug\publisher.exe: C:\Users\apoll\OneDrive\Desktop\SEM4\RPL\publisher\src\main.rs
